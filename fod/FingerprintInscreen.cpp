28eb4d1877770c27d4ab13964c1aaa1c6ead59d8
mdeejay <mdjrussia@gmail.com>
2019-08-30 16:32:35 +0200

    laurel_sprout: Update fod size & position
    [persist.vendor.sys.fp.device]: [F9S]
    [persist.vendor.sys.fp.expolevel]: [0xE0]
    [persist.vendor.sys.fp.fod.location.X_Y]: [293,1356]
    [persist.vendor.sys.fp.fod.size.width_height]: [134,134]
    [persist.vendor.sys.fp.info]: [0x0000003615050001]
    [persist.vendor.sys.fp.module]: [ofilm]
    [persist.vendor.sys.fp.mulexposupport]: [0x1]
    [persist.vendor.sys.fp.spmt]: [0x1]
    [persist.vendor.sys.fp.uid]: [51303956-33322E1-8F8880F8-CE000]
    [persist.vendor.sys.fp.vendor]: [goodix_fod]
    
    Signed-off-by: mdeejay <mdjrussia@gmail.com>

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -31,9 +31,9 @@
 #define FOD_STATUS_ON 1
 #define FOD_STATUS_OFF 0
 
-#define FOD_SENSOR_X 445
-#define FOD_SENSOR_Y 1931
-#define FOD_SENSOR_SIZE 190
+#define FOD_SENSOR_X 293
+#define FOD_SENSOR_Y 1356
+#define FOD_SENSOR_SIZE 134
 
 #define BRIGHTNESS_PATH "/sys/class/backlight/panel0-backlight/brightness"
 

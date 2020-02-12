3c94f09fb2ef87364cb1ab6f55cae12fafb42b80
HarukeyUA <inazarrusnak@gmail.com>
2020-02-12 14:49:06 +0200

    laurel_sprout: Drop light hal

--- device.mk
+++ device.mk
@@ -151,10 +151,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl
 
-# Lights
-PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.xiaomi_trinket
-
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles_vendor.xml:system/etc/media_profiles_vendor.xml
